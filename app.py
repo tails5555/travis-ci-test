f6b5b6070ef662618ae533c12eae7f9550f84b91
tails5555 <tails5555@naver.com>
2020-01-09 17:29:21 +0900

    HTTP Request 테스트 추가 Flask 가 더 간단해서 좋긴 하구나...

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, Flask!'
 
+@app.route('/home')
+def home_page():
+    return 'Home!'
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- test_sample.py
+++ test_sample.py
@@ -1,4 +1,18 @@
-from app import hello_world
+import requests
+from app import hello_world, home_page
 
+# 문자열 출력 테스트 (1)
 def test_hello_world() :
     assert 'Hello, Flask!' == hello_world()
+
+# 문자열 출력 테스트 (2)
+def test_home() :
+    assert 'Home!' == home_page()
+    
+# HTTP Request 테스트 (1)
+def test_request_hello_world() :
+    assert 'Hello, Flask!' == requests.get('http://127.0.0.1:5000').text
+
+# HTTP Request 테스트 (2)
+def test_request_home() :
+    assert 'Home!' == requests.get('http://127.0.0.1:5000/home').text
